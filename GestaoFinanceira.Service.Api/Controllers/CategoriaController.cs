1e36f721311d03c43bbb4247b6b03e813ebd4e04
JeffersonTavaresRJ <jef.tavaresrj@gmail.com>
2026-04-27 11:21:03 -0300

    27-04-2026: Teste Docker DSV - v04

--- GestaoFinanceira.Service.Api/Controllers/CategoriaController.cs
+++ GestaoFinanceira.Service.Api/Controllers/CategoriaController.cs
@@ -15,7 +15,7 @@ namespace GestaoFinanceira.Service.Api.Controllers
     [Route("api/[controller]")]
     [ApiController]
 
-    //03-teste ambiente desenvolvimento docker
+    //04-teste ambiente desenvolvimento docker
     public class CategoriaController : ControllerBase
     {
         private readonly ICategoriaApplicationService categoriaApplicationService;
